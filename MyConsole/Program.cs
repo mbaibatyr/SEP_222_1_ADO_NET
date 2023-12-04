3f45de538378791fc935ab5bd941a75b196f8373
mbaibatyr <murat_b@mail.ru>
2023-12-04 21:31:16 +0600

    --

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,15 +1,29 @@
-﻿using System.Data.SqlClient;
+﻿using Npgsql;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace MyConsole
 {
     internal class Program
     {
-        static string conStr_1 = "Server=214-P;Database=testDB;Trusted_Connection=True;";
-        static string conStr_2 = "Server=214-P;Database=testDB;User Id=user1;Password=1234;";
+        static string conStr_1 = "Server=207-P;Database=testDB;Trusted_Connection=True;";
+        static string conStr_2 = "Server=207-P;Database=testDB;User Id=user1;Password=1234;";
 
         static void TestConnection()
         {
             using (SqlConnection db = new SqlConnection(conStr_2))
+            {
+                db.Open();
+                Console.WriteLine(db.State.ToString());
+                db.Close();
+                Console.WriteLine(db.State.ToString());
+            }
+        }
+
+        static void TestConnectionPG()
+        {
+            using (NpgsqlConnection db = new NpgsqlConnection(conStr_2))
             {
                 db.Open();
                 Console.WriteLine(db.State.ToString());
@@ -26,7 +40,7 @@ namespace MyConsole
                 using (SqlCommand cmd = new SqlCommand("Select getdate()", db))
                 {
                     var ob = cmd.ExecuteScalar();
-                    if(ob != null)
+                    if (ob != null)
                         Console.WriteLine(ob.ToString());
                     else
                         Console.WriteLine("Empty");
@@ -44,21 +58,67 @@ namespace MyConsole
                 db.Open();
                 using (SqlCommand cmd = new SqlCommand("SELECT id, NAME FROM CITY", db))
                 {
-                   var dr = cmd.ExecuteReader();
+                    var dr = cmd.ExecuteReader();
                     while (dr.Read())
                     {
                         Console.WriteLine($"{dr[0].ToString()} {dr["NAME"].ToString()}");
                     }
-                }                
+                }
                 db.Close();
             }
         }
 
+        static void getCity2()
+        {
+            using (SqlConnection db = new SqlConnection(conStr_2))
+            {
+                db.Open();
+                DataTable dt = new DataTable();
+                using (SqlCommand cmd = new SqlCommand("SELECT id, NAME FROM CITY", db))
+                {
+                    dt.Load(cmd.ExecuteReader());
+                    //var res = dt.Select("name = 'Астана'");
+                    Console.WriteLine(dt.Rows.Count);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine($"{row[0].ToString()} {row["NAME"].ToString()}");
+                    }
+                }
+                db.Close();
+            }
+        }
+
+        static void pGetCityByName(string name)
+        {
+            using (SqlConnection db = new SqlConnection(conStr_2))
+            {
+                db.Open();
+                using (SqlCommand cmd = new SqlCommand("pGetCityByName", db))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("name", name);
+                    var dr = cmd.ExecuteReader();
+                    if(!dr.HasRows)
+                    {
+                        Console.WriteLine("NO DATA");
+                        return;
+                    }
+                    while (dr.Read())
+                    {
+                        Console.WriteLine($"{dr[0].ToString()} {dr["NAME"].ToString()}");
+                    }
+
+                }
+                db.Close();
+            }
+        }
         static void Main(string[] args)
         {
             //TestConnection();
             //getDate();
-            getCity();
+            //getCity();
+            //getCity2();
+            pGetCityByName("");
         }
     }
 }
